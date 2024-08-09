150b1660841774a475e43184b47ecfcaa4d8e6e5
AndreyBondarew <abondarew@gmail.com>
2024-08-09 11:18:15 +0200

    bugfix

--- lib/pages/home/part/common/search_field.dart
+++ lib/pages/home/part/common/search_field.dart
@@ -11,63 +11,66 @@ class _SearchField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomePageCubit, HomePageState>(
-      buildWhen: (previous, current) => previous.searchFieldHasFocus != current.searchFieldHasFocus,
-      builder: (context, state) {
-        return TextField(
-          focusNode: searchFieldFocusNode,
-          style: AppTextStyles.regular14(),
-          onChanged: (value) => context.read<HomePageCubit>().onChangeQuery(
-                value,
-              ),
-          onSubmitted: (value) => context.read<HomePageCubit>().submittedSearchQuery(value),
-          controller: searchFieldController,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: state.searchFieldHasFocus ? AppColors.layer3Color : AppColors.layer2Color,
-            hintText: state.searchFieldHasFocus ? null : AppStrings.searchFieldHint,
-            hintStyle: AppTextStyles.regular14(
-              color: AppColors.textPlaceholder,
-            ),
-            prefixIcon: Padding(
-              padding: const EdgeInsets.all(6.0),
-              child: AnimatedContainer(
-                duration: kThemeAnimationDuration,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: AppColors.layer1Color.withOpacity(state.searchFieldHasFocus ? 0 : 1),
+    return Padding(
+      padding: const EdgeInsets.only(top: 25.0),
+      child: BlocBuilder<HomePageCubit, HomePageState>(
+        buildWhen: (previous, current) => previous.searchFieldHasFocus != current.searchFieldHasFocus,
+        builder: (context, state) {
+          return TextField(
+            focusNode: searchFieldFocusNode,
+            style: AppTextStyles.regular14(),
+            onChanged: (value) => context.read<HomePageCubit>().onChangeQuery(
+                  value,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SvgPicture.asset(
-                    Assets.iconsSearch,
+            onSubmitted: (value) => context.read<HomePageCubit>().submittedSearchQuery(value),
+            controller: searchFieldController,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: state.searchFieldHasFocus ? AppColors.layer3Color : AppColors.layer2Color,
+              hintText: state.searchFieldHasFocus ? null : AppStrings.searchFieldHint,
+              hintStyle: AppTextStyles.regular14(
+                color: AppColors.textPlaceholder,
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: AnimatedContainer(
+                  duration: kThemeAnimationDuration,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: AppColors.layer1Color.withOpacity(state.searchFieldHasFocus ? 0 : 1),
                   ),
-                ),
-              ),
-            ),
-            suffixIcon: AnimatedOpacity(
-              duration: kThemeAnimationDuration,
-              opacity: state.searchFieldHasFocus ? 1 : 0,
-              child: Padding(
-                padding: const EdgeInsets.only(right: 10.0),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(30.0),
-                  onTap: state.searchFieldHasFocus
-                      ? () {
-                          searchFieldController.text = '';
-                          context.read<HomePageCubit>().onChangeQuery('');
-                        }
-                      : null,
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SvgPicture.asset(Assets.iconsClose),
+                    padding: const EdgeInsets.all(10.0),
+                    child: SvgPicture.asset(
+                      Assets.iconsSearch,
+                    ),
+                  ),
+                ),
+              ),
+              suffixIcon: AnimatedOpacity(
+                duration: kThemeAnimationDuration,
+                opacity: state.searchFieldHasFocus ? 1 : 0,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(30.0),
+                    onTap: state.searchFieldHasFocus
+                        ? () {
+                            searchFieldController.text = '';
+                            context.read<HomePageCubit>().onChangeQuery('');
+                          }
+                        : null,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset(Assets.iconsClose),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
